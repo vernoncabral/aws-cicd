e8cb288413040ba56c279d6f6ffec5ed84c1849c
Vernon Cabral <52911030+vernoncabral@users.noreply.github.com>
2025-10-30 00:04:30 +0530

    Update AwsDemoApplicationTests.java

--- src/test/java/com/javatechie/AwsDemoApplicationTests.java
+++ src/test/java/com/javatechie/AwsDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AwsDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
